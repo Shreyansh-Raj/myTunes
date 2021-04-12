a6e0b1c76082b30c84a1c0cbbf914dbf33843838
Hrishi Raj <hrishiraj1570@gmail.com>
2021-04-12 10:11:07 +0530

    Customized file layout

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 # Initializing flask app
@@ -9,8 +9,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# Making ContactMessage class for database
 class ContactMessage(db.Model):
-    pass
+    sno = db.Column(db.Integer,primary_key=True)
+    user_name = db.Column(db.String(120),nullable=False)
+    email = db.Column(db.String(200),nullable=False)
+    number = db.Column(db.String(15),nullable=False)
+    message = db.Column(db.String(500),nullable=False)
 
 # Serving home page to "/"
 @app.route("/")
@@ -22,9 +27,11 @@ def home():
 def playlist():
     return render_template('playlist.html')
 
-# Serving contact page to "/contact"
-@app.route("/contact")
+# Serving contact page to "/contact" and getting contact form post request
+@app.route("/contact",methods=['GET','POST'])
 def contact():
+    if request.method == 'POST':
+        pass
     return render_template("contact.html")
 
 # development code
